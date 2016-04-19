d9aca62661f4a97b102597f405ca56bbb96af41f
Marito <mariobrassescogarcia@gmail.com>
2016-04-19 15:45:06 +0200

    Add contacts, form and save new contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,4 +2,17 @@ class ContactsController < ApplicationController
 	def show
 		@contacts = Contact.all
 	end
+
+	def new
+	end
+
+	def add
+		@contact = Contact.create(
+			:name => params[:contact][:name],
+			:phonenumber => params[:contact][:phonenumber],
+			:address => params[:contact][:address],
+			:email => params[:contact][:email]
+			)
+		redirect_to("/")
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   get "/" => "contacts#show"
+  get "/new" => "contacts#new"
 
+  post "/add" => "contacts#add"
 
 
   # The priority is based upon order of creation: first created -> highest priority.

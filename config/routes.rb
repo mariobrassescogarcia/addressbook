44d0866c8a753c5a3b5a1ae6ea2442e399c6b7ef
Marito <mariobrassescogarcia@gmail.com>
2016-04-19 18:52:28 +0200

    Add favorite column to contacts. Migrate DB

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   get "/fail" => "contacts#fail"
 
+  get "/favorites" => "contacts#favorites"
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419123644) do
+ActiveRecord::Schema.define(version: 20160419164505) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160419123644) do
     t.string   "email"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.boolean  "favorite"
   end
 
 end
